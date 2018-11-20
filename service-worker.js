0996270ba0b9782b7c5e97c954fdd2dde16aa537
zhangzhiguo <zhangzhiguo010@outlook.com>
2018-11-20 22:31:37 +0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/resume-2018/build/precache-manifest.53cb571d09b48f89beb0af80abf05520.js"
+  "/resume-2018/build/precache-manifest.c4cb52f4e30bd894ce147cfb62e9502e.js"
 );
 
 workbox.clientsClaim();
